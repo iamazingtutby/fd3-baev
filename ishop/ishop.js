44ce0be609b0436e37f2953e25ae214bcb2f3816
iamazingtutby <i-amazing@tut.by>
2017-12-04 01:08:18 +0300

    ishop v1

---
+++ ishop/ishop.js
@@ -0,0 +1,72 @@
+var ishopTable = React.createClass({
+
+    displayName: 'ishopTable',
+
+    propTypes: {
+        goods:React.PropTypes.arrayOf(
+          React.PropTypes.shape({
+              name: React.PropTypes.string.isRequired,
+              price: React.PropTypes.string.isRequired,
+              photo: React.PropTypes.string.isRequired,
+              stock: React.PropTypes.string.isRequired,
+              sku: React.PropTypes.string.isRequired
+          })
+        )
+    },
+
+    render: function() {
+
+        var goodsTable = [];
+        var goodsTable2 = [];
+
+        for (var i = 0; i < this.props.goods.length; i += 3) {
+            goodsTable2.push(this.props.goods.slice(i, i + 3));
+        }
+        console.log(goodsTable2);
+
+        goodsTable2.forEach( function (v) {
+                var tds = [];
+                v.forEach(function (item) {
+                    var goodsTd =
+                        React.DOM.td({key:item.sku, className:'item'},
+                            React.DOM.div({className: 'card'},
+                                React.DOM.img({className:'photo card-img-top', src: item.photo}),
+                                React.DOM.div({className: 'card-body'},
+                                    React.DOM.h4({className:'card-title'},item.name),
+                                    React.DOM.div({className:'price'},
+                                        React.DOM.span({},'Цена '),
+                                        React.DOM.span({},item.price)
+                                        ),
+                                    React.DOM.div({className:'stock'},
+                                        React.DOM.span({},'Остаток '),
+                                        React.DOM.span({},item.stock)),
+                                    React.DOM.span({className:'sku'},
+                                        React.DOM.span({},'Артикул '),
+                                        React.DOM.span({},item.sku))
+                                    )
+                                )
+                        )
+                    tds.push(goodsTd);
+                })
+
+                var goodsTr =
+                    React.DOM.tr({key:v.sku,className:'item-row'},
+                        tds
+                    );
+                goodsTable.push(goodsTr);
+        }
+
+        );
+        return React.DOM.div(
+            {className:'ishopTable'},
+            React.DOM.table(
+                {className:'table'},
+                React.DOM.tbody(
+                    {className: 'tbody'},
+                        goodsTable
+                )
+            )
+        );
+    },
+
+});
